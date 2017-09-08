108f76d25fa03459f0e5dc093577e10b5e630712
martaoto <martaotomarta@gmail.com>
2017-09-08 02:51:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var countStars = function () {
+	var checkedRadio = document.querySelector(".stars input:checked");
+	console.log("checkedRadio.value");
+};
+
+document.querySelector(".stars").addEventListener("click", countStars)
